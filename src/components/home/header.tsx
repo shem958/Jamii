20dea1478c77a6f9c2904b2d14810e8978845c2d
shem958 <shemgikunda37@gmail.com>
2025-02-13 19:22:18 +0300

    Enhance HeroSection and Header components with responsive design and mobile menu functionality

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,51 +1,155 @@
-import React from "react";
-import { Box, Button, Stack, Typography, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Stack,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import Image from "next/image";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Header: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box
       sx={{
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        px: 4,
+        px: { xs: 2, md: 4 },
         py: 2,
-        backgroundColor: "primary.main",
-        color: "common.white",
+        backgroundColor: "common.white",
+        color: "primary.main",
+        borderBottom: "1px solid #f0f0f0",
       }}
     >
-      <Stack direction="row" spacing={2} alignItems="center">
-        <Image
-          src="/assets/logo-icon.svg"
-          alt="logo"
-          width={40}
-          height={40}
-          priority
-        />
-        <Typography variant="h6">Jamii Cereals and Gen Shop</Typography>
-      </Stack>
-      <Stack direction="row" spacing={2} alignItems="center">
-        <Button variant="text" color="inherit">
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: 600,
+          fontSize: "1.25rem",
+          color: "primary.main",
+          "&:hover": {
+            cursor: "pointer",
+          },
+        }}
+      >
+        Jamii Cereals and Gen shop
+      </Typography>
+
+      {/* Desktop Navigation */}
+      <Stack
+        direction="row"
+        spacing={4}
+        alignItems="center"
+        sx={{
+          display: { xs: "none", md: "flex" },
+        }}
+      >
+        <Button
+          endIcon={<KeyboardArrowDownIcon />}
+          sx={{
+            color: "primary.main",
+            textTransform: "none",
+            fontWeight: 500,
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+          }}
+        >
           Products
         </Button>
-        <Button variant="text" color="inherit">
+
+        <Button
+          endIcon={<KeyboardArrowDownIcon />}
+          sx={{
+            color: "primary.main",
+            textTransform: "none",
+            fontWeight: 500,
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+          }}
+        >
           Company
         </Button>
-        <Button variant="contained" color="secondary">
-          Get in Touch
+
+        <Button
+          sx={{
+            color: "primary.main",
+            textTransform: "none",
+            fontWeight: 500,
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+          }}
+        >
+          Frequently Asked Questions
         </Button>
 
-        <IconButton>
-          <Image
-            src="/assets/menu-icon.svg"
-            alt="Menu"
-            width={30}
-            height={30}
-            priority
-          />
-        </IconButton>
+        <Button
+          variant="contained"
+          color="secondary"
+          sx={{
+            textTransform: "none",
+            borderRadius: "4px",
+            px: 3,
+            py: 1,
+            color: "common.white",
+            "&:hover": {
+              backgroundColor: "secondary.dark",
+            },
+          }}
+        >
+          Get in Touch
+        </Button>
       </Stack>
+
+      {/* Mobile Menu Icon */}
+      <IconButton
+        onClick={handleMenuOpen}
+        sx={{
+          display: { xs: "flex", md: "none" },
+          padding: 0,
+        }}
+      >
+        <Image
+          src="/assets/log06.png"
+          alt="Menu"
+          width={30}
+          height={30}
+          priority
+        />
+      </IconButton>
+
+      {/* Mobile Menu */}
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+        sx={{
+          display: { xs: "block", md: "none" },
+        }}
+      >
+        <MenuItem onClick={handleMenuClose}>Products</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Company</MenuItem>
+        <MenuItem onClick={handleMenuClose}>
+          Frequently Asked Questions
+        </MenuItem>
+        <MenuItem onClick={handleMenuClose}>Get in Touch</MenuItem>
+      </Menu>
     </Box>
   );
 };
